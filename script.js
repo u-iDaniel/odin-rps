b787abb480b155eacc15e0dcfd90c4b1b7c75d50
Daniel <167367785+u-iDaniel@users.noreply.github.com>
2024-05-24 21:57:24 -0600

    Add human choice functionality

--- script.js
+++ script.js
@@ -2,3 +2,12 @@ function getComputerChoice() {
     let choice = ["rock", "paper", "scissors"];
     return choice[Math.floor(Math.random() * 3)];
 }
+
+function getHumanChoice() {
+    const choice = ["rock", "paper", "scissors"];
+    let humanChoice = (prompt("Choose a move (rock/paper/scissors)")).toLocaleLowerCase();
+    while (!choice.includes(humanChoice)) {
+        humanChoice = (prompt("Please choose a valid move (rock/paper/scissors)")).toLocaleLowerCase();
+    }
+    return humanChoice;
+}
