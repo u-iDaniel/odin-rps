2c907b29581466989b8f060cd5efafe247dbe762
Daniel <167367785+u-iDaniel@users.noreply.github.com>
2024-05-24 22:28:45 -0600

    Add game logic
    Test game logic by playing for one round.

--- script.js
+++ script.js
@@ -14,3 +14,26 @@ function getHumanChoice() {
     }
     return humanChoice;
 }
+
+function playRound(humanChoice, computerChoice) {
+    const choice = ["rock", "paper", "scissors"];
+    console.log(`Computer chose: ${computerChoice}`);
+    
+    const humanChoiceIndex = choice.indexOf(humanChoice);
+    const computerChoiceIndex = choice.indexOf(computerChoice);
+
+    if (humanChoiceIndex === (computerChoiceIndex + 1) % 3) {  // Winning (e.g. scissors beats paper)
+        console.log(`You win! ${choice[humanChoiceIndex].toLocaleUpperCase()} beats ${choice[computerChoiceIndex].toLocaleUpperCase()}.`);
+        humanScore++;
+    } else if (computerChoiceIndex === (humanChoiceIndex + 1) % 3) {  // Losing (e.g. scissors loses paper)
+        console.log(`You lose! ${choice[computerChoiceIndex].toLocaleUpperCase()} beats ${choice[humanChoiceIndex].toLocaleUpperCase()}.`);
+        computerScore++;
+    } else if (computerChoiceIndex === humanChoiceIndex) {  // Drawing (e.g. scissors draws against scissors)
+        console.log(`You draw! Both players chose ${choice[computerChoiceIndex].toLocaleUpperCase()}.`)
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
