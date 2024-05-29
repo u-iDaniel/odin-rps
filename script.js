8f64821888209efef5976e4e0cfa565df4d37311
Daniel <167367785+u-iDaniel@users.noreply.github.com>
2024-05-28 21:34:36 -0600

    Refactor irrelevant code
    Delete human choice function as there are buttons. Remove global score
    counter to add later

--- script.js
+++ script.js
@@ -4,14 +4,6 @@ function getComputerChoice() {
     return choice[Math.floor(Math.random() * 3)];
 }
 
-function getHumanChoice() {
-    let humanChoice = (prompt("Choose a move (rock/paper/scissors)")).toLocaleLowerCase();
-    while (!choice.includes(humanChoice)) {
-        humanChoice = (prompt("Please choose a valid move (rock/paper/scissors)")).toLocaleLowerCase();
-    }
-    return humanChoice;
-}
-
 function playRound(humanChoice, computerChoice) {
     console.log(`Computer chose: ${computerChoice}`);
     
@@ -20,20 +12,15 @@ function playRound(humanChoice, computerChoice) {
 
     if (humanChoiceIndex === (computerChoiceIndex + 1) % 3) {  // Winning (e.g. scissors beats paper)
         console.log(`You win! ${choice[humanChoiceIndex].toLocaleUpperCase()} beats ${choice[computerChoiceIndex].toLocaleUpperCase()}.`);
-        humanScore++;
     } else if (computerChoiceIndex === (humanChoiceIndex + 1) % 3) {  // Losing (e.g. scissors loses to rock)
         console.log(`You lose! ${choice[computerChoiceIndex].toLocaleUpperCase()} beats ${choice[humanChoiceIndex].toLocaleUpperCase()}.`);
-        computerScore++;
     } else if (computerChoiceIndex === humanChoiceIndex) {  // Drawing (e.g. scissors draws against scissors)
         console.log(`You draw! Both players chose ${choice[computerChoiceIndex].toLocaleUpperCase()}.`);
     }
-
-    console.log("Current scores: ");
-    console.log(`Player score: ${humanScore}`);
-    console.log(`Computer score: ${computerScore}`);
 }
 
 let body = document.querySelector('body');
+
 body.addEventListener("click", (event) => {
     playRound(event.target.id, getComputerChoice());
 });
